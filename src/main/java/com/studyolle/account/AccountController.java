33836fc50dcf5504c6e26b1a734471aafb6fda16
Lee  Chanho <sma05091@nate.com>
2023-10-02 23:54:16 +0900

    회원가입 view 작성

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -9,6 +9,7 @@ public class AccountController {
 
     @GetMapping("/sign-up")
     public String SignUpForm(Model model) {
+        model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
 }
