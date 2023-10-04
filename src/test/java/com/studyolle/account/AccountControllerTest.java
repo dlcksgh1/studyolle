9dc525584921ee7761f1b690cf211721b9c52bc4
Lee  Chanho <sma05091@nate.com>
2023-10-05 00:09:50 +0900

    회원가입 테스트

--- src/test/java/com/studyolle/account/AccountControllerTest.java
+++ src/test/java/com/studyolle/account/AccountControllerTest.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -19,6 +25,12 @@ class AccountControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    AccountRepository accountRepository;
+
+    @MockBean
+    JavaMailSender javaMailSender;
+
     @Test
     @DisplayName("회원가입 폼 테스트")
     public void signUpForm() throws Exception {
@@ -28,5 +40,36 @@ class AccountControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("account/sign-up"))
                 .andExpect(model().attributeExists("signUpForm"));
+    }
+
+    @Test
+    @DisplayName("회원 가입 처리: 입력값 오류")
+    public void signUpSubmitWithError() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .param("nickname", "nickname")
+                        .param("email", "test@email")
+                        .param("password", "1234")
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+
+    @Test
+    @DisplayName("회원 가입 처리 : 입력값 정상")
+    public void signUpSubmit() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .param("nickname", "nickname")
+                        .param("email", "test@gmail.com")
+                        .param("password", "123445678")
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+
+        assertTrue(accountRepository.existsByEmail("test@gmail.com"));
+
+        then(javaMailSender).should().send(any(SimpleMailMessage.class));
+
     }
 }
