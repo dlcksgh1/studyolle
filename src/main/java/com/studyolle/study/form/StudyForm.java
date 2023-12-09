6b6caaf68e7f8c1e2c37b1f32bc5e5fe49d58731
Lee  Chanho <sma05091@nate.com>
2023-12-09 19:15:55 +0900

    스터디 도메인, 스터디 개설 테스트코드

--- src/main/java/com/studyolle/study/form/StudyForm.java
+++ src/main/java/com/studyolle/study/form/StudyForm.java
@@ -1,12 +1,18 @@
 package com.studyolle.study.form;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class StudyForm {
 
     @NotBlank
