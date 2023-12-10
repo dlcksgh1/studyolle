3484bb450c8017ca60801ca8ff63f65744ed34c2
Lee  Chanho <sma05091@nate.com>
2023-12-10 11:02:04 +0900

    스터디설정 - 배너 테스트코드

--- src/test/java/com/studyolle/study/StudySettingsControllerTest.java
+++ src/test/java/com/studyolle/study/StudySettingsControllerTest.java
@@ -16,7 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -80,4 +80,51 @@ class StudySettingsControllerTest {
         assertEquals(fullDescriptionToBeUpdated, study.getFullDescription());
     }
 
+
+    @Test
+    @DisplayName("스터디 세팅 폼 조회(배너)")
+    @WithAccount("test123")
+    void studySettingFormBanner() throws Exception {
+        mockMvc.perform(get("/study/" + studyPath + "/settings/banner"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("study/settings/banner"))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("study"));
+    }
+
+    @Test
+    @DisplayName("스터디 배너 업데이트")
+    @WithAccount("test123")
+    void updateStudyBanner() throws Exception {
+        mockMvc.perform(post("/study/" + studyPath + "/settings/banner")
+                        .param("image", "image-test")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/study/" + studyPath + "/settings/banner"));
+    }
+
+
+    @Test
+    @DisplayName("스터디 배너 사용")
+    @WithAccount("test123")
+    void enableStudyBanner() throws Exception {
+        mockMvc.perform(post("/study/" + studyPath + "/settings/banner/enable")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/study/" + studyPath + "/settings/banner"));
+        Study study = studyRepository.findByPath(studyPath);
+        assertTrue(study.isUseBanner());
+    }
+
+    @Test
+    @DisplayName("스터디 배너 미사용")
+    @WithAccount("test123")
+    void disableStudyBanner() throws Exception {
+        mockMvc.perform(post("/study/" + studyPath + "/settings/banner/disable")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/study/" + studyPath + "/settings/banner"));
+        Study study = studyRepository.findByPath(studyPath);
+        assertFalse(study.isUseBanner());
+    }
 }
