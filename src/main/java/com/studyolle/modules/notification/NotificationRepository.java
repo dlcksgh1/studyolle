32f22d28554bf62ecdb8e566d9e79e1e8f9f09bc
Lee  Chanho <sma05091@nate.com>
2023-12-30 11:06:45 +0900

    알림 목록 조회 및 삭제

--- src/main/java/com/studyolle/modules/notification/NotificationRepository.java
+++ src/main/java/com/studyolle/modules/notification/NotificationRepository.java
@@ -2,8 +2,18 @@ package com.studyolle.modules.notification;
 
 import com.studyolle.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     Long countByAccountAndChecked(Account account, boolean check);
+
+    @Transactional
+    List<Notification> findByAccountAndCheckedOrderByCreatedDateTimeDesc(Account account, boolean checked);
+
+    @Transactional
+    void deleteByAccountAndChecked(Account account, boolean checked);
 }
 
